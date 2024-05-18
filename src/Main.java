152d68e85aa210d88eb557668bf92b4b86e12d1b
mahnoor-arain <mehranahemdsoomro@gmail.com>
2024-05-18 21:37:27 +0500

    Update Main.java
    Adding a Integer

--- src/Main.java
+++ src/Main.java
@@ -4,3 +4,5 @@ public class GuessTheNumber {
     public static void main(String[] args) {
  Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1;
+        int numberOfTries = 0;
