ebb93b705834490d3fb1b28b4fdd5928be85d829
Mehran-ahmed <mehrankhansoomro339@gmail.com>
2024-05-18 21:34:51 +0500

    Update Main.java
    Adding Scanner

--- src/Main.java
+++ src/Main.java
@@ -2,3 +2,5 @@ import java.util.Random;
 import java.util.Scanner;
 public class GuessTheNumber {
     public static void main(String[] args) {
+ Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
