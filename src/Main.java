66a917d88a0df2c2509968cb1817f7e10cfea5f8
Mehran-ahmed <mehrankhansoomro339@gmail.com>
2024-05-18 22:30:06 +0500

    Update Main.java
    Added Full Game Code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class GuessTheNumber {
     public static void main(String[] args) {
- Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int numberToGuess = random.nextInt(100) + 1;
         int numberOfTries = 0;
@@ -11,11 +12,12 @@ public class GuessTheNumber {
 
         System.out.println("Welcome to the Guess The Number game!");
         System.out.println("I have selected a number between 1 and 100. Can you guess it?");
-         while (!win) {
+
+        while (!win) {
             System.out.print("Enter your guess: ");
             guess = scanner.nextInt();
             numberOfTries++;
-             
+
             if (guess < 1 || guess > 100) {
                 System.out.println("Please enter a number between 1 and 100.");
             } else if (guess < numberToGuess) {
@@ -24,10 +26,11 @@ public class GuessTheNumber {
                 System.out.println("Too high. Try again.");
             } else {
                 win = true;
-            System.out.println("Congratulations! You've guessed the number.");
+                System.out.println("Congratulations! You've guessed the number.");
                 System.out.println("It took you " + numberOfTries + " tries.");
             }
         }
 
         scanner.close();
     }
+}
