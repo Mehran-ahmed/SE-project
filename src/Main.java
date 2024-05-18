100dd599220e2ac0dc0193d53068f5cda71e4bdd
Mehran-ahmed <mehrankhansoomro339@gmail.com>
2024-05-18 22:32:05 +0500

    Update Main.java
    added whole code

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,42 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumber {
     public static void main(String[] args) {
+        final int MIN = 1;
+        final int MAX = 100;
+
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int numberToGuess = random.nextInt(100) + 1;
+        int numberToGuess = random.nextInt(MAX - MIN + 1) + MIN;
         int numberOfTries = 0;
-        int guess;
+        int guess = 0;
         boolean win = false;
 
         System.out.println("Welcome to the Guess The Number game!");
-        System.out.println("I have selected a number between 1 and 100. Can you guess it?");
+        System.out.println("I have selected a number between " + MIN + " and " + MAX + ". Can you guess it?");
 
         while (!win) {
             System.out.print("Enter your guess: ");
-            guess = scanner.nextInt();
-            numberOfTries++;
+            try {
+                guess = scanner.nextInt();
+                numberOfTries++;
 
-            if (guess < 1 || guess > 100) {
-                System.out.println("Please enter a number between 1 and 100.");
-            } else if (guess < numberToGuess) {
-                System.out.println("Too low. Try again.");
-            } else if (guess > numberToGuess) {
-                System.out.println("Too high. Try again.");
-            } else {
-                win = true;
-                System.out.println("Congratulations! You've guessed the number.");
-                System.out.println("It took you " + numberOfTries + " tries.");
+                if (guess < MIN || guess > MAX) {
+                    System.out.println("Please enter a number between " + MIN + " and " + MAX + ".");
+                } else if (guess < numberToGuess) {
+                    System.out.println("Too low. Try again.");
+                } else if (guess > numberToGuess) {
+                    System.out.println("Too high. Try again.");
+                } else {
+                    win = true;
+                    System.out.println("Congratulations! You've guessed the number.");
+                    System.out.println("It took you " + numberOfTries + " tries.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter an integer.");
+                scanner.next(); // Clear the invalid input
             }
         }
 
